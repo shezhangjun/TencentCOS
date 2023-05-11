1465695c66b4322448c554731df5f8430a3027c9
shezhangjun <v_yyjwang@tencent.com>
2023-05-11 17:30:38 +0800

    增加通过COS多版本功能快速批量恢复数据脚本

---
+++ Python_SDK/COS_Disaster_Recovery/DisasterRecovery.py
@@ -0,0 +1,124 @@
+# -*- coding=utf-8
+# author shezhangjun
+# Python3.x
+import pandas as pd
+from qcloud_cos import CosConfig
+from qcloud_cos import CosS3Client
+
+
+copied_object_list = []
+copied_object_mtime = []
+copied_object_versionid_list = []
+
+
+def _get_bucket_lists(src_region, src_secret_id, src_secret_key, src_bucket, prefix, keymarker, versionidmarker,
+                      maxkeys):  # 获取存储桶内的所有对象及其历史版本信息
+    client = CosS3Client(CosConfig(Region=src_region, SecretId=src_secret_id, SecretKey=src_secret_key))
+    responselist = client.list_objects_versions(
+        Bucket=src_bucket,
+        Prefix=prefix,
+        Delimiter='',
+        KeyMarker=keymarker,
+        VersionIdMarker=versionidmarker,
+        MaxKeys=maxkeys,
+        EncodingType=''
+    )
+    lists = responselist
+    return lists
+
+
+def _copy_object_to_dst_bucket(dst_region, dst_secret_id, dst_secret_key, src_region, dst_bucket, dst_objectkey,
+                               src_bucket, src_objectkey, versionId):
+    client = CosS3Client(CosConfig(Region=dst_region, SecretId=dst_secret_id, SecretKey=dst_secret_key))
+    client.copy(
+        Bucket=dst_bucket,
+        Key=dst_objectkey,
+        CopySource={
+            'Bucket': src_bucket,
+            'Key': src_objectkey,
+            'Region': src_region,
+            'VersionId': versionId
+        }
+    )
+
+
+def _copied_object(obejectkey):
+    data = {'key': copied_object_list}
+    df = pd.DataFrame(data)
+    is_exist = obejectkey in df['key'].values
+    return is_exist
+
+
+def _recover_main(src_region, src_secret_id, src_secret_key, src_bucket, prefix, keymarker, versionidmarker, maxkeys,
+                  dst_region, dst_secret_id, dst_secret_key, dst_bucket):
+    i = 0
+    while True:
+        response = _get_bucket_lists(src_region, src_secret_id, src_secret_key, src_bucket, prefix, keymarker,
+                                     versionidmarker, maxkeys)
+        if 'Version' in response:  # 标记不是删除标记的记录
+            for k in response['Version']:
+                i += 1
+                versionId = k['VersionId']
+                key = k['Key']
+                expectedLength = k['Size']
+                expectedEtag = k['ETag']
+                islatest = k['IsLatest']
+                owner = k['Owner']
+                getStorageClass = k['StorageClass']
+                mtime = k['LastModified']
+                print(f'Id:{i}')  # ID
+                print(f'versionId:{versionId}')  # 版本ID
+                print(f'key:{key}')  # 对象全称
+                print(f'islatest:{islatest}')  # 是否最新
+                print(f'expectedLength:{expectedLength}')  # 对象大小
+                print(f'expectedEtag:{expectedEtag}')  # Etag值
+                print(f'Owner:{owner["ID"]}:{owner["DisplayName"]}')  # 资源拥有者ID和名称
+                print(f'getStorageClass:{getStorageClass}')  # 对象类型
+                print(f'LastModified:{mtime}')
+                if islatest == 'true':  # 标记是最新的记录
+                    print(f'最新版文件,拷贝key:{key},version:{versionId}')
+                    _copy_object_to_dst_bucket(dst_region, dst_secret_id, dst_secret_key, src_region, dst_bucket,
+                                               key, src_bucket, key, versionId)
+                    copied_object_list.append(key)
+                    copied_object_mtime.append(mtime)
+                    copied_object_versionid_list.append(versionId)
+                else:
+                    if not _copied_object(obejectkey=key):  # 判断此文件是否被拷贝过,如已拷贝则跳过不复制
+                        print(f'次新版文件,拷贝key:{key},version:{versionId}')
+                        _copy_object_to_dst_bucket(dst_region, dst_secret_id, dst_secret_key, src_region, dst_bucket,
+                                                   key, src_bucket, key, versionId)
+                        copied_object_list.append(key)
+                        copied_object_mtime.append(mtime)
+                        copied_object_versionid_list.append(versionId)
+
+                    else:
+                        print(f'文件已被拷贝复制，此处跳过')
+        if response['IsTruncated'] == 'false':
+            break
+        keymarker = response['NextKeyMarker']
+        versionidmarker = response['NextVersionIdMarker']
+    print('--------------------------------------')
+    print("++++++++++++++++++++++++++++++++++++++++++++")
+    print("++++++++++++++++++++++++++++++++++++++++++++")
+    print("已复制的对象列表")
+    print(copied_object_list)
+    data = {'key': copied_object_list, 'versionid': copied_object_versionid_list, 'LastModified': copied_object_mtime}
+    df = pd.DataFrame(data)
+    df.to_csv('已复制的对象列表.csv', index=False)
+
+
+if __name__ == '__main__':
+    src_region = ''  # 误删桶文件的地域
+    src_secret_id = ''  # 误删桶文件的SecretID
+    src_secret_key = ''  # 误删桶文件的SecretID
+    src_bucket = ''  # 误删文件的桶名
+    dst_region = ''  # 待恢复文件的地域
+    dst_secret_id = ''  # 待恢复文件桶的SecretID
+    dst_secret_key = ''  # 待恢复文件桶的SecretKey
+    dst_bucket = ''  # 待恢复文件的桶名
+    prefix = ''  # 设置要恢复的对象前缀
+    keymarker = ''  # 默认为空，勿动此参数
+    versionidmarker = ''  # 默认为空，勿动此参数
+    maxkeys = 1000
+    _recover_main(src_region, src_secret_id, src_secret_key, src_bucket, prefix, keymarker, versionidmarker, maxkeys,
+                  dst_region, dst_secret_id, dst_secret_key, dst_bucket)
---
+++ Python_SDK/COS_Disaster_Recovery/README.md
@@ -0,0 +1,12 @@
+# TencentCOS
+腾讯云对象存储SDK-Python
+
+## 使用Python SDK 结合COS多版本功能快速批量恢复数据
+
+
+### 使用方法
+
+* 推荐Python3.x
+* 先安装腾讯云对象存储[SDK-Python](https://cloud.tencent.com/document/product/436/12269)
+* git clone https://github.com/shezhangjun/TencentCOS.git
+* 在[DisasterRecovery.py](https://github.com/shezhangjun/TencentCOS/blob/master/Python_SDK/COS_Disaster_Recovery/DisasterRecovery.py) 文件中最下方配置存储桶名以及密钥信息
